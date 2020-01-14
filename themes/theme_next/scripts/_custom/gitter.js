04082d5cf23e61caab75928290b0b4b1f3e3332b
leon Qiu <hotseason007@gmail.com>
2020-01-14 20:41:56 +0800

    add gitter

--- themes/theme_next/scripts/_custom/gitter.js
+++ themes/theme_next/scripts/_custom/gitter.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log("test custom script");
-
 hexo.extend.filter.register("theme_inject", function(injects) {
   injects.head.file("gitter", "views/gitter.swig", {}, { cache: true });
   injects.style.push("views/gitter.styl");
